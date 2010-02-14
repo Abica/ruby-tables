f4daf9e6aa1961ea804426d84d8082bf4e8caf5b
Nicholas Wright <nick.loves.rails@gmail.com>
2010-02-13 19:21:14 -0700

    remove unused code from spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,25 +1,4 @@
 require File.join( File.dirname( __FILE__ ), "..", "lib", "table" )
  
-=begin
-module Table
-  module Test
-    module Factory
-      def create_backend_record( name = :temp )
-        Packit::Backend.create name do | record |
-          if block_given?
-            yield record
-          else
-            record.add :field_1
-            record.add :field_2
-            record.add :field_3
-          end
-        end
-      end
-    end
-  end
-end
-=end
- 
-Spec::Runner.configure do |config|
-  #config.include Table::Test::Factory
+Spec::Runner.configure do | config |
 end
